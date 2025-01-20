f6d96670acebf26b1915dfd1ffc05601559901ce
Abii06 <abiramiarasu626@gmail.com>
2025-01-20 16:09:06 +0530

    Adding Gallery and Contact Components

--- seceFrontend/src/App.jsx
+++ seceFrontend/src/App.jsx
@@ -1,12 +1,19 @@
 import ClassCompEg from "./components/Class components/ClassCompEG"
 import About from "./components/Functional components/About"
 import Home from "./components/Functional components/Home"
+import Gallery from "./components/Functional components/Gallery"
+import Contact from "./components/Functional components/Contact"
 function App(){
   return(
     <div>
-      <Home/>
-      <About/>
-      <ClassCompEg />
+      <Home/><hr/>
+      <About/><hr/>
+      <ClassCompEg /><hr/>
+      <h2>Without Props</h2>
+      <Gallery/>
+      <h2>With Props</h2>
+      <Gallery image="SECE LOGO" page="Gallery"/><hr/>
+      <Contact/>
     </div>
   )
 }
--- seceFrontend/src/components/Functional components/Gallery.jsx
+++ seceFrontend/src/components/Functional components/Gallery.jsx
@@ -1,7 +1,9 @@
-const Gallery = () =>{
+const Gallery = (props) =>{
     return(
         <div>
-            <h2>Gallery Page</h2>
+            <h2>{props.page} Page</h2>
+            <img src="./src/assets/logo.png" alt="SECE LOGO"/>
+            <h2>The image is {props.image}</h2>
         </div>
     )
 }
