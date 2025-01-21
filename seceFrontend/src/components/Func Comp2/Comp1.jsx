07a8e595b904a689f5f6bdd6fe3934e4192c6beb
Abii06 <abiramiarasu626@gmail.com>
2025-01-21 18:23:28 +0530

    Creating func comp

---
+++ seceFrontend/src/components/Func Comp2/Comp1.jsx
@@ -0,0 +1,8 @@
+const Comp1 = () => {
+    return(
+        <div>
+        <h2 style={{textAlign:"center"}}>This is Functional Component 1</h2>
+        </div>
+    )
+};
+export default Comp1;
---
+++ seceFrontend/src/components/Func Comp2/Comp2.jsx
@@ -0,0 +1,8 @@
+const Comp2 = () => {
+    return(
+        <div>
+        <h2 style={{textAlign:"center"}}>This is Functional Component 2</h2>
+        </div>
+    )
+};
+export default Comp2;
